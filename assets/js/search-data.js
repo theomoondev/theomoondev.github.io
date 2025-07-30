a9d1c60360d7ceca3beab7d3276bab832f92bcf0
theomoondev <theo.y.s.moon@gmail.com>
2025-07-30 07:48:58 +0000

    Deploying to gh-pages from @ theomoondev/theomoondev.github.io@9db472792d25be9b31f3d05fc6b146bd061caf7b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,29 +30,29 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/repositories/";
           },
-        },{id: "projects-attribute-migration-strategy",
-          title: 'Attribute Migration Strategy',
-          description: "Sample of a technical strategy for migrating an undisclosed model attribute from one service to another",
+        },{id: "projects-service-feature-migration-strategy",
+          title: 'Service Feature Migration Strategy',
+          description: "Sample of a technical strategy for migrating a feature from an undisclosed legacy service",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-feature-decomposition-strategy",
-          title: 'Feature Decomposition Strategy',
-          description: "Sample of a technical strategy for decomposing an undisclosed feature from a service",
+            },},{id: "projects-model-attribute-decomposition-strategy",
+          title: 'Model Attribute Decomposition Strategy',
+          description: "Sample of a technical strategy for decomposing an attribute from an undisclosed object model",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-service-feature-description",
-          title: 'Service Feature Description',
-          description: "Sample of a technical description for an undisclosed service feature",
+            },},{id: "projects-object-creation-procedures",
+          title: 'Object Creation Procedures',
+          description: "Sample of the systematic steps and procedures involved in creating a new instance of an object within an undisclosed service",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-service-feature-runbooks",
-          title: 'Service Feature Runbooks',
-          description: "Sample of two runbooks describing E2E testing and manual creation with video walkthroughs for an undisclosed service feature",
+            },},{id: "projects-service-runbooks",
+          title: 'Service Runbooks',
+          description: "Sample of two service runbooks describing E2E testing and manual creation procedures with video walkthroughs",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
             },},{id: "projects-taskpie-github-readme",
           title: 'TaskPie GitHub README',
-          description: "A GitHub README featuring a recorded demo, installation guide, dependencies, and Trello board",
+          description: "A GitHub README featuring a recorded demo, user stories, installation guide, dependencies, and Trello board",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
             },},{id: "projects-i-kill-plants-but-i-want-to-grow-a-vegetable-garden",
