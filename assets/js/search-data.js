f7df5c9d41e62d4c012e9b758f09a5308343478a
theomoondev <theo.y.s.moon@gmail.com>
2025-08-05 08:50:52 +0000

    Deploying to gh-pages from @ theomoondev/theomoondev.github.io@21b5eeb79052fe46c50b2db98964acc74b3aa19c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,24 +32,19 @@ ninja.data = [{
           },
         },{id: "projects-traveladvisory-api",
           title: 'TravelAdvisory API',
-          description: "Sample of REST API documentation created using an OpenAPI specification in YAML format. The interactive HTML view was generated using Redocly.",
+          description: "Sample of a REST API reference written as an OpenAPI specification in YAML format, presented as an interactive HTML view using Redocly",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-service-feature-migration-strategy",
-          title: 'Service Feature Migration Strategy',
-          description: "Sample of a technical strategy for migrating a feature from a generalized legacy service",
+            },},{id: "projects-legacy-api-migration-strategy",
+          title: 'Legacy API Migration Strategy',
+          description: "Sample of a developer-facing technical strategy for migrating a collection of API endpoints from a generalized legacy service",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-instance-creation-procedures",
-          title: 'Instance Creation Procedures',
-          description: "Sample of the systematic steps and procedures involved in creating a new instance of an object within a generalized service",
+            },},{id: "projects-instance-creation-overview-amp-runbooks",
+          title: 'Instance Creation Overview &amp;amp; Runbooks',
+          description: "Sample of a developer-facing document that provides an overview on creating a new object instance within a generalized service, along with two service runbooks describing E2E testing and manual creation procedures",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-service-runbooks",
-          title: 'Service Runbooks',
-          description: "Sample of two service runbooks describing E2E testing and manual creation procedures with video walkthroughs",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
             },},{id: "projects-taskpie-github-readme",
           title: 'TaskPie GitHub README',
           description: "A GitHub README featuring a recorded demo, user stories, installation guide, dependencies, and Trello board",
@@ -57,7 +52,7 @@ ninja.data = [{
               window.location.href = "/projects/5_project/";
             },},{id: "projects-i-kill-plants-but-i-want-to-grow-a-vegetable-garden",
           title: 'I KILL PLANTS But I Want to Grow a Vegetable Garden',
-          description: "Sample of a detailed, practical guide for growing your own vegetable garden",
+          description: "Sample of a detailed, practical guide for growing your own vegetable garden with custom layouts, drawings, and diagrams",
           section: "Projects",handler: () => {
               window.location.href = "/projects/6_project/";
             },},{
